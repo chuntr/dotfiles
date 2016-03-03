73d058ccdee566931790ab8d1ff25700940b880a
Christopher Hunter <chunter@denormal.org>
2016-03-03 12:18:28 -0800

    added log parse functions and updated git aliases

--- .log_parse_functions.sh
+++ .log_parse_functions.sh
@@ -2,4 +2,6 @@
 
 # $1 - site name/seach term
 # $2 - filename
+
 alias blog="grep $1 local3.2016-01-21 | tail -20 | sed -re 's/(.*)( \"Mozilla.*)/\1/' | grep httperror"
+alias clog="grep $1 $2 | sed -e 's|\(.*http\: \)\(.*\)\(\[.*\] \)\(.*\)\( \"Mozilla.*\)|\1\2\4|'"
